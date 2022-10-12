ea2f2bf808ef2a338a087b61a557780c314f28bc
火线兔 <silverhawk@foxmail.com>
2022-10-12 17:48:44 +0800

    test

--- request.go
+++ request.go
@@ -387,7 +387,7 @@ func (r *Request) request(method, url string, data ...interface{}) (*Response, e
 	r.initCookies(req)
 	r.initBasicAuth(req)
 
-	resp, err := r.cli.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
